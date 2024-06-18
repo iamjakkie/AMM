1c867be5ce62cd0089b32c4a0a5af65fc5d5770c
jakkie <jakubkielbasiewicz@gmail.com>
2024-06-18 23:50:29 +0200

    calculate swap returns

--- contracts/AMM.sol
+++ contracts/AMM.sol
@@ -70,6 +70,14 @@ contract AMM {
         return (_token2Amount * token1Balance) / token2Balance;
     }
 
+    function calculateToken1Swap(
+        uint256 _token1Amount
+    ) public view returns (uint256 token2Amount) {
+        uint256 token1After = token1Balance + _token1Amount;
+        uint256 token2After = K / token1After;
+        token2Amount = token2Balance - token2After;
+    }
+
     function swapToken1(
         uint256 _token1Amount
     ) external returns (uint256 token2Amount) {
